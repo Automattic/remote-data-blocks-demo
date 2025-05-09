a99360478f60c0da2cb30eac895d0e97338513dc
Jacob Wayne Smith <jacob.smith@automattic.com>
2025-05-09 14:35:54 -0700

    basics

--- car-locator/build/render.php
+++ car-locator/build/render.php
@@ -1,9 +1,38 @@
 <?php declare(strict_types = 1);
 
-namespace RemoteDataBlocks\Example\Airtable\LeafletMap;
+namespace RemoteDataBlocks\Demo\CarLocator;
 
+// Retrieve the query from the registry.
+$car_location_query = CarLocatorRegistry::$query;
 
-$coordinates = [["x" => 46.188567, "y" => -123.830123, "name" => "Car 1"]];
+// Ensure the query is available before trying to use it.
+if ( ! $car_location_query instanceof \RemoteDataBlocks\Config\Query\HttpQuery ) {
+	?>
+<div
+	<?php echo get_block_wrapper_attributes(); ?> >
+	<p>Car location data is currently unavailable.</p>	
+	</div>
+<?php
+	return;
+}
+
+$response = $car_location_query->execute( [] );
+$coordinates = [];
+
+if ( ! is_wp_error( $response ) ) {
+	$coordinates = array_map( function ( $value ) {
+		$result = $value['result'];
+		return [
+			'x' => $result['latitude']['value'],
+			'y' => $result['longitude']['value'],
+			'type' => $result['vehicle_type']['value'],
+			'charge' => $result['charge_percentage']['value'],
+			'status' => $result['status']['value'],
+		];
+	}, $response['results'] );
+}
+
+//$coordinates = [["x" => 46.188567, "y" => -123.830123, "name" => "Car 1"]];
 
 
 ?>
--- car-locator/car-map.php
+++ car-locator/car-map.php
@@ -7,7 +7,18 @@
  * Author: WordPress VIP and Cascade AI
  */
 
-namespace RemoteDataBlocks\Example\Airtable\LeafletMap;
+namespace RemoteDataBlocks\Demo\CarLocator;	
+
+use RemoteDataBlocks\Config\DataSource\HttpDataSource;
+use RemoteDataBlocks\Config\Query\HttpQuery;
+
+/**
+ * Registry class to hold the car location query.
+ */
+class CarLocatorRegistry {
+	/** @var \RemoteDataBlocks\Config\Query\HttpQuery|null */
+	public static $query = null;
+}
 
 function register_leaflet_map_block(): void {
 
@@ -18,5 +29,60 @@ function register_leaflet_map_block(): void {
 
 	// Register the block using the build artifact.
 	register_block_type( __DIR__ . '/build' );
+
+	
+	$car_location_data_source = HttpDataSource::from_array( [
+		'display_name' => 'Car Locations',
+		'endpoint' =>'https://dummyjson.com/c/8b75-8395-46b9-9633',
+		'request_headers' => [
+			'Content-Type' => 'application/json',
+		],
+	] );
+
+	if ( !$car_location_data_source instanceof HttpDataSource ) {
+		return;
+	}
+
+	$car_location_query = HttpQuery::from_array([
+		'data_source' => $car_location_data_source,
+		'output_schema' => [
+			'is_collection' => true,
+			'type' => [
+				'id' => [
+					'name' => 'ID',
+					'path' => '$.id',
+					'type' => 'string',
+				],
+				'latitude' => [
+					'name' => 'Latitude',
+					'path' => '$.coordinates.latitude',
+					'type' => 'number',
+				],
+				'longitude' => [
+					'name' => 'Longitude',
+					'path' => '$.coordinates.longitude',
+					'type' => 'number',
+				],
+				'vehicle_type' => [
+					'name' => 'Vehicle Type',
+					'path' => '$.vehicle_type',
+					'type' => 'string',
+				],
+				'charge_percentage' => [
+					'name' => 'Charge Percentage',
+					'path' => '$.charge_percentage',
+					'type' => 'number',
+				],
+				'status' => [
+					'name' => 'Status',
+					'path' => '$.status',
+					'type' => 'string',
+				],
+			],
+		],
+	]);
+
+	CarLocatorRegistry::$query = $car_location_query;
+
 }
 add_action( 'init', __NAMESPACE__ . '\\register_leaflet_map_block' );

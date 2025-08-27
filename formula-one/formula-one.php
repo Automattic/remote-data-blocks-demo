6c4204ec9bcecb4f1faccb60553b640a109c8039
Chris Zarate <chris.zarate@automattic.com>
2025-08-27 12:57:27 -0600

    Coerce `team_id` to string before passing to `rawurlencode`

--- formula-one/formula-one.php
+++ formula-one/formula-one.php
@@ -331,7 +331,7 @@ function formula_one_rdb_init(): void {
 		'endpoint' => function ( array $input_variables ) use ( $f1_data_source ): string {
 			$endpoint = $f1_data_source->get_endpoint() . '/teams';
 			if ( ! empty( $input_variables['team_id'] ) ) {
-				$endpoint .= '?id=' . rawurlencode( $input_variables['team_id'] );
+				$endpoint .= '?id=' . rawurlencode( strval( $input_variables['team_id'] ) );
 			}
 			return $endpoint;
 		},
